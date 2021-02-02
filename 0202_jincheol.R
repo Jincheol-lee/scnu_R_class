7872f3beb6177864a5b7e80f1abae300e923255b
Jincheol-lee <averagel@naver.com>
2021-02-02 03:28:40 -0500

    2월 2일 R프로그램 pm5.28

--- 0202_jincheol.R
+++ 0202_jincheol.R
@@ -25,7 +25,7 @@ var00
 #논리 연산자
 x <- TRUE        #x 변수에 TRUE를 저장
 y <- FALSE       #y 변수에 FALSE를 저장
-z <- TRUE      #블록지정해서 컨트롤+엔터해야함
+z <- TRUE #블록지정해서 한번 컨트롤+엔터해야함
 v <- FALSE
 
 x | y # OR 연산 (저 작대기는 엔터키 위)
@@ -38,7 +38,82 @@ x & z # AND 연산(둘다 진실이면 진실)
              #괄호가 다른 결과를 준다고 함
 
 !x  #NOT연산 (x는 트루이기 떄문에 답은 페일)
-!y
+!y   #y은 거짓이기 떄문에 낫연산 트루임
+
+
+var = 1
+var <- 1
+
+var1
+var2
+        #c=컨텍트레이즈 (연결시키다)
+var3 <- c(1,2,3,4,5) #(각각의 요인 케이스)
+var3   #를 사용하면 여러개를 넣으수 있음
+var4 <- c("김진영","2")
+        #글자를 널을떄는 ""사용(문자로 인식)
+
+# "2"+4 = 못구함 
+# 컴터는 "2"를 문자로 인식
+
+var5 <- seq(1,10)
+var5
+ #사용자가 콤마를 누르고 숫자를 누르는것을
+ #중간에 실수를 방지하기 위해 만들어진
+ #seq() 를 사용하면 됨(순서대로 만들기)
+  
+  ##연속 데이터 생성 하기(위에꺼)
+
+var6 <- seq(1,10, by=2)
+var6
+ # 중간에 2의 배수가 사라짐
+
+  ## 일정 간격으로 연속 데이터 생성하기(위)
+
+var7 <- rep(var6, times=2)
+var7
+ #rep 반복하라 times는 횟수
+
+  ##반복해서 생성하기
+
+var8 <- rep(var6, each=2)
+var8
+
+##### 논리 연산자 ############
+  # 무조건 & -> |
+ #(TRUE | TRUE) & FALSE # FALSE
+ #TRUE | (TRUE & FALSE) # TRUE
+ #TRUE | TRUE & FALSE # TRUE
+
+ #(FALSE & TRUE) | TRUE # TRUE
+ #FALSE & (TRUE | TRUE) # FALSE
+ #FALSE & TRUE | TRUE   # FALSE
+
+  # 예상 결과
+ #(TRUE | FALSE) & FALSE #FALSE
+ #TRUE | (FALSE & FALSE) #TRUE
+ #TRUE | FALSE & FALSE #TRUE
+
+################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
